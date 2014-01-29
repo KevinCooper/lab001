4964e77cbc0cc4d229410df9ff16017b8380f2a5
Kevin Cooper <boo2atfoo@gmail.com>
2014-01-29 10:22:58 -0700

    Initial draft of the v_sync_gen.  No testing completed and no significant errors encountered beyond syntax errors.

--- v_sync_gen.vhd
+++ v_sync_gen.vhd
@@ -17,9 +17,51 @@ entity v_sync_gen is
 end v_sync_gen;
 
 architecture Cooper of v_sync_gen is
-
+	type states is (activeVideo, frontPorch, sync, backPorch);
+	signal state_reg, state_next: states;
+	signal clock_state, clock_next: unsigned(1000);
 begin
 
-
+	-- state register
+   process(clk,reset)
+   begin
+      if (reset='1') then
+         state_reg <= start;
+			clock_state <= unsigned(0);
+      elsif (clk'event and clk='1') then
+         state_reg <= state_next;
+			clock_state <= clock_next;
+      end if;
+   end process;
+	
+	--Next State Logic
+	process(input, clock_state, h_sync) is
+	begin
+		state_next <= state_next;
+		clock_next <= clock_next;
+		if(clock_state = 525) then
+			state_next <= activeVideo;
+			clock_next <= 0;
+		elsif(clock_state = 480) then
+			state_next <= frontPorch;
+		elsif(clock_state = 490) then
+			state_next <= sync;
+		elsif(clock_state = 492) then
+			state_next <= backPorch;
+		end if;
+		if(h_completed = '1') then
+			clock_next <= clock_state +1;
+		end if;
+	end process;
+	
+	--Output Logic
+	v_sync <= '1' when state_reg = sync else
+				 '0';
+	blank  <= '0' when state_reg = activeVideo else
+	          '1';
+	row <= clock_state when clock_state < 480 else
+				 "000000000";
+	completed <= '1' when clock_state = 525 else
+					 '0';
 end Cooper;
 
