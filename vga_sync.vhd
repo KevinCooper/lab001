ed88e76dcbd7fd4021f9ae90b0a118ec3ca92c8f
Kevin Cooper <boo2atfoo@gmail.com>
2014-02-03 20:33:37 -0700

    Hooked everything up in the VGA_SYNC module.  No testing done yet though.

--- vga_sync.vhd
+++ vga_sync.vhd
@@ -16,10 +16,67 @@ entity vga_sync is
      );
 end vga_sync;
 
+
 architecture Cooper of vga_sync is
 
+component v_sync_gen
+    port ( clk         : in  std_logic;
+           reset       : in std_logic;
+           h_blank     : in std_logic;
+           h_completed : in std_logic;
+           v_sync      : out std_logic;
+           blank       : out std_logic;
+           completed   : out std_logic;
+           row         : out unsigned(10 downto 0)
+     );
+end component;
+
+component h_sync_gen
+    port ( clk       : in  std_logic;
+           reset     : in  std_logic;
+           h_sync    : out std_logic;
+           blank     : out std_logic;
+           completed : out std_logic;
+           column    : out unsigned(10 downto 0)
+     );
+end component;
+
+signal wire_column, wire_row: unsigned(10 downto 0);
+signal wire_hsync, wire_vsync, wire_vcompleted: std_logic;
+signal wire_vblank, wire_hblank: std_logic;
+signal wire_hcompleted: std_logic;
 begin
 
+	my_v : v_sync_gen port map
+	(
+	clk=> clk,
+	reset=>reset,
+	h_blank=>wire_hblank,
+	h_completed=>wire_hcompleted,
+	v_sync=>wire_vsync,
+	blank=>wire_vblank,
+	completed=>wire_vcompleted,
+	row=>wire_row
+	);
+	
+	my_h : h_sync_gen port map
+	(
+	clk=> clk,
+	reset=>reset,
+	h_sync=>wire_hsync,
+	blank=>wire_hblank,
+	completed=>wire_hcompleted,
+	column=>wire_column
+	);
+
+--output signals
+row <= wire_row;
+column<=wire_column;
+blank <= '0' when wire_hblank='0' and wire_vblank='0'
+			else '1';
+h_sync<=wire_vsync;
+v_sync<=wire_hsync;
+v_completed<=wire_vcompleted;
 
 end Cooper;
 
